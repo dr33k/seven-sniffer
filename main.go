ffab2c086141057e78c6dfc4225b9596cc1238e2
EmelieC <emeliec@hardcorebiometric.com>
2026-03-06 16:37:14 +0100

    Added infinite loop for goroutines to keep trying to connect to an address with a 1 secon delay on failure

--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
-type packet struct{
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
 
+type address struct{
+	Host string
+	Port string
 }
 
-func sniff(ip string, packets chan *packet){
-
+type packet struct {
+	Ch chan bool
+	IP string
 }
 
-func main(){
-	packets:= make(chan *packet)
-	ip:= "192.0.0.1"
+type ipStatus struct{
+	Ad *address
+	IsOnline bool
+	Err string
+}
 
-	for range 5{
-		go sniff(ip, packets)
+func sniff(ad *address, packets chan *packet, statuses chan *ipStatus, timeoutSeconds uint8 ) {
+	for{
+		conn, err := net.Dial("tcp", fmt.Sprintf("%v:%v", ad.Host, ad.Port))
+
+		if err != nil{
+			statuses <- &ipStatus{Ad:ad, IsOnline: false, Err: err.Error()}
+			time.Sleep(1 * time.Second) //chill
+			continue
+		}
+
+		statuses <- &ipStatus{Ad:ad, IsOnline: true, Err: ""}
+		reader := bufio.NewReader(conn)
+		for {
+			
+		}
+	}
+}
+
+func main() {
+	packets := make(chan *packet)
+	statuses := make(chan *ipStatus)
+
+	ip := "192.0.0.1"
+
+	for port:= range 5 {
+		go sniff(&address{ip, string(port)}, packets, statuses, 60)
 	}
 
-	for p := range packets{
-
+	for p := range packets {
+		fmt.Println(p)
 	}
 }
